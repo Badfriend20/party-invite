e0b1adf5d8c2363c5103bd852828f95a811a696b
Job Alberto <badfriend_20@hotmail.com>
2024-02-01 17:38:52 -0600

    Se agrega archivo config para constantes

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+export const MessageTypes = {
+    doble: "doble",
+    initialText: "texto inicial",
+    checkList: "lista win lose",
+    address: "direccion",
+    invite: "presenta invitacion"
+}
+
+export const adjustment = 0;
