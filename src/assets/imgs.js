82325280921dbcec3aa6d8941d6a9cac2b6d5595
Job Alberto <badfriend_20@hotmail.com>
2024-02-01 17:37:40 -0600

    Se agrega ruta de boton ver ubicacion

--- src/assets/imgs.js
+++ src/assets/imgs.js
@@ -1,11 +1,13 @@
 import { default as backgroundBanner } from "./imgs/background.svg";
 import { default as jobBanner } from "./imgs/job.svg";
 import { default as laloBanner } from "./imgs/lalo.svg";
+import { default as addressButton } from "./imgs/boton.svg";
 
 const images = {
     backgroundBanner,
     jobBanner,
-    laloBanner
+    laloBanner,
+    addressButton
 }
 
 export default images;
