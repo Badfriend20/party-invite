6690e35b7a817d248bb0b9f9d19ab507a4a20122
Job Alberto <badfriend_20@hotmail.com>
2024-01-31 22:23:58 -0600

    Probando posicionamiento de los numeros

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -8,20 +8,20 @@ const Background = () => {
     return (
         <>
             <img src={images.backgroundBanner} alt="background" />
-            
-            <Number position={{ left: 85, top: '64.3vh'}} number={1} />
-            <Number position={{ left: 173, top: '64.3vh' }} number={2} />
 
-            <Number position={{ left: 290, top: '64.3vh'}} number={1} />
-            <Number position={{ left: 375, top: '64.3vh' }} number={2} />
+            <Number position={{ left: 85, top: 899 }} number={1} />
+            <Number position={{ left: 173, top: 899 }} number={2} />
 
-            <Number position={{ left: 85, top: '78vh'}} number={1} />
-            <Number position={{ left: 173, top: '78vh' }} number={2} />
+            <Number position={{ left: 290, top: 899 }} number={3} />
+            <Number position={{ left: 375, top: 899 }} number={4} />
 
-            <Number position={{ left: 290, top: '78vh'}} number={1} />
-            <Number position={{ left: 375, top: '78vh' }} number={2} />
+            <Number position={{ left: 85, top: 1015 }} number={5} />
+            <Number position={{ left: 173, top: 1015 }} number={6} />
+
+            <Number position={{ left: 290, top: 1015 }} number={7} />
+            <Number position={{ left: 375, top: 1015 }} number={8} />
         </>
-        
+
     )
 }
 
