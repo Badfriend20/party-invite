b0d48e88048146f6c4722ee57acb45794839e433
Job Alberto <badfriend_20@hotmail.com>
2024-01-31 19:43:18 -0600

    Agregando swipper a package

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,19 +4,14 @@ import Number from './Number'
 
 const Background = () => {
 
-    const style = {
-        maxWidth: "320px",
-        marginRight: "auto",
-        marginLeft: "auto",
-        position: "relative"
-    }
+
 
     return (
-        <div style={style}>
+        <>
             <img src={images.backgroundBanner} alt="background" />
-            <Number position={{ left: 56, top: 108}} number={1} />
-            <Number position={{ left: 113, top: 108 }} number={2} />
-        </div>
+            {/* <Number position={{ left: 56, top: 108 }} number={1} />
+            <Number position={{ left: 113, top: 108 }} number={2} /> */}
+        </>
     )
 }
 
---
+++ src/components/SliderBanner.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SliderBanner = () => {
+  return (
+    <div style={{
+      width: "100%",
+      height: "45vh",
+      backgroundColor: "#e5e5e5",
+      padding: "10px"
+    }}>
+      Yo habia ponido una imagen aqui
+    </div>
+  )
+}
+
+export default SliderBanner
