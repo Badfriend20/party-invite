53745c0866a88d85060df106931a7d448fa104ef
Job Alberto <badfriend_20@hotmail.com>
2024-02-01 19:04:08 -0600

    Se modifica url de mapa

--- src/components/items/AddressButton.jsx
+++ src/components/items/AddressButton.jsx
@@ -5,7 +5,7 @@ import images from '../../assets/imgs'
 const AddressButton = ({ position }) => {
     return (
         <PositioningWrapper position={position}>
-            <a href="https://www.google.com/maps/@19.44974,-99.1785095,3a,75y,78.09h,118.16t/data=!3m6!1e1!3m4!1sTc0gOzCgshdWkBUU_v_6aQ!2e0!7i16384!8i8192?entry=ttu">
+            <a href="https://www.google.com/maps/search/?api=1&query=19.449746043358466, -99.17848158809478">
                 <img width={"250px"} src={images.addressButton} />
             </a>
         </PositioningWrapper>
