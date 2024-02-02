2a1114532a34d61c1f4f2d15c758a0e7731c04ca
Job Alberto <badfriend_20@hotmail.com>
2024-02-01 19:05:03 -0600

    Se agrega logica de contador

--- src/components/wrappers/Countdown.jsx
+++ src/components/wrappers/Countdown.jsx
@@ -1,21 +1,65 @@
-import React from 'react'
-import Number from '../Items/Number'
+import React, { useEffect, useState } from 'react';
+import DateGroups from './DateGroups';
+import { DATE_TARGET } from '../../config/config';
 
 const Countdown = () => {
+
+
+
+    const MILLISECONDS_OF_A_SECOND = 1000;
+    const MILLISECONDS_OF_A_MINUTE = MILLISECONDS_OF_A_SECOND * 60;
+    const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
+    const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24;
+
+    function updateCountdown(DATE_TARGET) {
+
+        const NOW = new Date()
+        const DURATION = DATE_TARGET - NOW;
+        const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY);
+        const REMAINING_HOURS = Math.floor((DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR);
+        const REMAINING_MINUTES = Math.floor((DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE);
+        const REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND);
+
+        return DURATION > 0 ? {
+            REMAINING_DAYS,
+            REMAINING_HOURS,
+            REMAINING_MINUTES,
+            REMAINING_SECONDS
+        } : {
+            REMAINING_DAYS: 0,
+            REMAINING_HOURS: 0,
+            REMAINING_MINUTES: 0,
+            REMAINING_SECONDS: 0
+        }
+    }
+
+    const [counter, setCounter] = useState(updateCountdown(DATE_TARGET));
+
+    useEffect(() => {
+        const interval = setInterval(() => { setCounter(updateCountdown(DATE_TARGET)) }, MILLISECONDS_OF_A_SECOND);
+        if (
+            counter.REMAINING_DAYS == 0 &&
+            counter.REMAINING_HOURS == 0 &&
+            counter.REMAINING_MINUTES == 0 &&
+            counter.REMAINING_SECONDS == 0
+        ) {
+            clearInterval(interval)
+        }
+        return () => {
+            clearInterval(interval);
+        }
+    }, [])
+
+
     return (
         <>
 
-            <Number position={{ left: 85, top: 935 }} number={1} />
-            <Number position={{ left: 173, top: 935 }} number={2} />
+            <DateGroups positions={[{ left: 85, top: 935 }, { left: 173, top: 935 }]}>{counter.REMAINING_DAYS}</DateGroups>
+            <DateGroups positions={[{ left: 290, top: 935 }, { left: 375, top: 935 }]}>{counter.REMAINING_HOURS}</DateGroups>
+            <DateGroups positions={[{ left: 85, top: 1052 }, { left: 173, top: 1052 }]}>{counter.REMAINING_MINUTES}</DateGroups>
+            <DateGroups positions={[{ left: 290, top: 1052 }, { left: 375, top: 1052 }]}>{counter.REMAINING_SECONDS}</DateGroups>
 
-            <Number position={{ left: 290, top: 935 }} number={3} />
-            <Number position={{ left: 375, top: 935 }} number={4} />
-
-            <Number position={{ left: 85, top: 1052 }} number={5} />
-            <Number position={{ left: 173, top: 1052 }} number={6} />
-
-            <Number position={{ left: 290, top: 1052 }} number={7} />
-            <Number position={{ left: 375, top: 1052 }} number={8} /></>
+        </>
     )
 }
 
