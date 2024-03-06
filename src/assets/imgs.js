c9a84440ea3b9d93654a3c3828d0f5e7af4b7c9f
Lalo <lalogen1@gmail.com>
2024-03-05 20:16:05 -0600

    png

--- src/assets/imgs.js
+++ src/assets/imgs.js
@@ -1,6 +1,6 @@
 import { default as backgroundBanner } from "./imgs/background.svg";
-import { default as jobBanner } from "./imgs/job.svg";
-import { default as laloBanner } from "./imgs/lalo.svg";
+import { default as jobBanner } from "./imgs/job.png";
+import { default as laloBanner } from "./imgs/lalo.png";
 import { default as addressButton } from "./imgs/boton.svg";
 
 const images = {
